4103a034010c17164c89ec2f15f5476fc0f27674
Jenel-web <markjenelcortas29@gmail.com>
2026-04-08 22:30:14 +0800

    Added CorsConfig for debugging

--- src/main/java/com/MovieBookingApplication/MJCinema/Config/CorsConfig.java
+++ src/main/java/com/MovieBookingApplication/MJCinema/Config/CorsConfig.java
@@ -9,18 +9,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 // this class will be used to manipulate the CORS since there is an error with the CORS mapping
 //which leads to the deployed frontend not being able to access the backend.
 @Configuration
-public class CorsConfig {
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("https://mjcinemaapp.vercel.app") // Use your main Vercel URL
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        };
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://mjcinemaapp.vercel.app")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
